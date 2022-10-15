84c83d07bb5e594e25a028c7ab547a20dbf31be8
Md saikat <111490728+Saikat1428@users.noreply.github.com>
2022-10-15 10:02:11 +0600

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    double d;
+    float f;
+    char c;
+    printf("size of int = %d",sizeof(i));
+    printf("size of int = %d",sizeof(d);
+    printf("size of int = %d",sizeof(f);
+    printf("size of int = %d",sizeof(c);
+    getch();
+}
